e99ee615dd5491b16ee4cd987efdd828ed09b0e0
luciaviji15-lang <lucia.viji15@gmail.com>
2026-09-13 16:37:43 +0200

    feat: fallo Render puerto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-
   app.enableCors();
 
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
